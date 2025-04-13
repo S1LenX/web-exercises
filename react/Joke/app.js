c0b8d2e019379c6013c93e127c6a2e84b21aaeea
S1LenX <varsh31v@gmail.com>
2025-04-13 21:04:07 +0530

    Create app.js

---
+++ react/Joke/app.js
@@ -0,0 +1,15 @@
+import Header from './q1';
+import Content from './q2';
+import Footer from './q3';
+
+function App() {
+  return (
+    <div>
+      <Header title="Welcome to My React App" />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
