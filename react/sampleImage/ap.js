f21570d9506ed2e7b89040b71450ae8317f7d301
S1LenX <varsh31v@gmail.com>
2025-04-13 20:08:01 +0530

    Create ap.js

---
+++ react/sampleImage/ap.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PublicImage = () => {
+  return (
+    <div>
+      <h2>Image from Public Folder</h2>
+      <img src="/Images/sample.jpg" alt="Public Folder" width="300" />
+    </div>
+  );
+};
+
+export default PublicImage;
