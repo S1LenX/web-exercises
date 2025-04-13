3499492a69c93422b1343c553d487d8b915a287e
S1LenX <varsh31v@gmail.com>
2025-04-13 19:50:19 +0530

    Create app.js

---
+++ react/sumofsquares/app.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SumOfSquares = () => {
+  const a = 3, b = 4;
+  const sum = a * a + b * b;
+  return <p>Sum of squares: {sum}</p>;
+};
+
+export default SumOfSquares;
