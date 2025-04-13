10f6529364af777e6c693f5b380b69bd9d20c1d3
S1LenX <varsh31v@gmail.com>
2025-04-13 21:00:30 +0530

    Create ProfileImage.js

---
+++ react/ProfilePage/ProfileImage.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ProfileImage = () => {
+  return (
+    <div>
+      <img
+        src="https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg"
+        alt="Profile"
+        width="150"
+        height="150"
+      />
+    </div>
+  );
+};
+
+export default ProfileImage;
