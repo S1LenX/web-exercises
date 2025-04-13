88d931cf4e0c7e3202ef6cae55d9506106269f34
S1LenX <varsh31v@gmail.com>
2025-04-13 19:59:17 +0530

    Create app.js

---
+++ react/userGreeting/app.js
@@ -0,0 +1,8 @@
+import React from "react";
+import UserGreeting from "./usergreeting";
+
+function App() {
+  return <UserGreeting firstName="Varsh" lastName="Vishwakarma" />;
+}
+
+export default App;
