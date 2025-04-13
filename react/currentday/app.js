dcceba413fb76b128f86ff6c608a47bef625f7f4
S1LenX <varsh31v@gmail.com>
2025-04-13 19:51:59 +0530

    Create app.js

---
+++ react/currentday/app.js
@@ -0,0 +1,7 @@
+const CurrentDay = () => {
+  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const today = new Date().getDay();
+  return <h1>Today is {days[today]}</h1>;
+};
+
+export default CurrentDay;
