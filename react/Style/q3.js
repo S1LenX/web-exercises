d85bbdca6922d12b815133126eef66eb8916e98a
S1LenX <varsh31v@gmail.com>
2025-04-13 21:07:59 +0530

    Create q3.js

---
+++ react/Style/q3.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles.css';
+
+function StyledButtonExternal() {
+  return <button className="external-button">Click Me (External)</button>;
+}
+
+export default StyledButtonExternal;
