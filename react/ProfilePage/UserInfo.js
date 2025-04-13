8c5f615dada366dab51df3c2ef7ee7a86a723173
S1LenX <varsh31v@gmail.com>
2025-04-13 21:00:05 +0530

    Create UserInfo.js

---
+++ react/ProfilePage/UserInfo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const UserInfo = () => {
+  return (
+    <div>
+      <h2>Varsh Vishwakarma</h2>
+      <p>Email: varsh.Vishwakarma@example.com</p>
+      <p>Bio: A passionate React developer.</p>
+    </div>
+  );
+};
+
+export default UserInfo;
