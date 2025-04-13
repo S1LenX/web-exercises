68217c70b0fffe0d7164e4a04516d5773dbecbd6
S1LenX <varsh31v@gmail.com>
2025-04-13 21:07:04 +0530

    Create app.js

---
+++ react/Style/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import StyledButtonInline from './q1';
+import StyledButtonInternal from './q2';
+import StyledButtonExternal from './q3';
+
+function App() {
+  return (
+    <div>
+      <h2>React Button Styling Examples</h2>
+      <StyledButtonInline />
+      <StyledButtonInternal />
+      <StyledButtonExternal />
+    </div>
+  );
+}
+
+export default App;
