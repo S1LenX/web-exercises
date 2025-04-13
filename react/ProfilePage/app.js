d8f530e9d99953bd560fac91da62c73b4ba0026f
S1LenX <varsh31v@gmail.com>
2025-04-13 20:57:07 +0530

    Create app.js

---
+++ react/ProfilePage/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ProfilePage from "./ProfilePage";
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>React Components Demo</h1>
+      <ProfilePage />
+    </div>
+  );
+}
+
+export default App;
