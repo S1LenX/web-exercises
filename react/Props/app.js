ca343dce09e90124e1b5be3d0f95784d82907e54
S1LenX <varsh31v@gmail.com>
2025-04-13 21:13:01 +0530

    Create app.js

---
+++ react/Props/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Parent from './q2';
+
+function App() {
+  return (
+    <div>
+      <h1>Props and Props Validation Example</h1>
+      <Parent />
+    </div>
+  );
+}
+
+export default App;
